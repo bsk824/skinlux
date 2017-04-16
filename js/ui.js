3c90b9bf62eb2795cb02807e911df6f50af5f250
seungki <bsk824@gmail.com>
2017-04-17 01:56:47 +0900

    no message

--- js/ui.js
+++ js/ui.js
@@ -42,6 +42,18 @@ $(function() {
 		});
 	}
 	$('#gnb').gnb();
+	
+	$(".datepicker").datepicker({
+		changeYear: true,
+		changeMonth: true,
+		showMonthAfterYear: true,
+		yearSuffix: "년",
+		prevText: "이전달",
+		nextText: "다음달",
+		dateFormat: "yy / mm / dd",
+		dayNamesMin: ["일","월","화","수","목","금","토"],
+		monthNamesShort: ["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"]
+	});
 });
 
 function tabContent(obj) {
